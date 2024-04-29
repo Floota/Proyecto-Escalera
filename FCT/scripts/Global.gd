c22c9a6dc693d9d09a9a6a73bbe686b8ebcfef61
Floota <151873135+Floota@users.noreply.github.com>
2024-04-29 20:10:17 +0200

    Add files via upload

---
+++ FCT/scripts/Global.gd
@@ -0,0 +1,15 @@
+extends Node
+
+
+
+#HP, Puntuación, Posición, Frutas, Tiempo para guardar.
+
+#Variables a guardar: Tiempo, frutas, puntuación, HP. 
+
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
--- FCT/scripts/Player.gd
+++ FCT/scripts/Player.gd
@@ -3,7 +3,8 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-
+const FRICTION = 1500.0
+var JUMP_VELOCITY = -400.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -12,6 +13,25 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+
+func apply_gravity(delta):
+	global.player_pos_y = position.y
+	if not is_on_floor():
+		velocity.y += gravity*1.1 * delta
+		velocity.x*0.8
+		global.on_floor = false;
+
+func handle_air():
+	if is_on_floor() or coyote_jump_timer.time_left < 0.0:
+		global.on_floor = true
+		hover_ready = true
+		global.hovering = false
+		if Input.is_action_just_pressed("ui_accept"):
+			velocity.y = JUMP_VELOCITY
+	else:
+		#Shorthop
+		if Input.is_action_just_released("ui_accept") and velocity.y < JUMP_VELOCITY / 2:
+			velocity.y = JUMP_VELOCITY / 2
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
